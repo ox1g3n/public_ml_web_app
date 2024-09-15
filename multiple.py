6fc6e4d3d946ac7872fd16df7cc72e8e3a9caff9
ox1g3n <130974471+ox1g3n@users.noreply.github.com>
2024-09-15 20:54:47 +0530

    multiple.py

--- multiple.py
+++ multiple.py
@@ -93,10 +93,12 @@ if 'Diabetes' in selected_option:
 
         if diab_prediction[0] == 1:
             diab_diagnosis = 'The person is diabetic'
+            st.error(diab_diagnosis)
         else:
             diab_diagnosis = 'The person is not diabetic'
+            st.success(diab_diagnosis)
 
-    st.success(diab_diagnosis)
+    
 
 elif 'Heart' in selected_option:
     st.title('Heart Disease prediction selected.')
